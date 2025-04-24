3bd9b52da27489cf77ebc3b17618014cd2c10a4a
paulammoun <paul@pcs-lb.com>
2025-04-24 11:17:08 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 // **Template code last revised: 08/30/21
 
-const staticCacheName = 'MobilevisitPWAN4_pwa_cache-v-01.10';
-const dynamicCacheName = 'MobilevisitPWAN4_pwa_dynamic_cache-v-01.10';
+const staticCacheName = 'MobilevisitPWAN4_pwa_cache-v-01.11';
+const dynamicCacheName = 'MobilevisitPWAN4_pwa_dynamic_cache-v-01.11';
 const dynamicCacheSize = 10;
 const offlineFilename = './offline.html';
 const showDebugInfo = true;
